d8f0dbe3a96811438ba366716cfd58cf18ed1c4d
GraceYu <librayuyueran@gmail.com>
2020-10-12 09:43:51 -0500

    use canvas draw lines

--- Canvas/action.js
+++ Canvas/action.js
@@ -1,19 +1,47 @@
-
 let canvas = document.getElementById('canvas')
+// canvas.onmouseover = (e) => {
+//   console.log(e)
+//   console.log(e.clientX)
+//   console.log(e.clientY)
+//   let div = document.createElement('div')
+//   div.style.position = 'absolute'
+//   div.style.left = e.clientX + 'px'
+//   div.style.top = e.clientY + 'px'
+//   div.style.width = '5px'
+//   div.style.height = '5px'
+//   div.style.marginLeft = '-3px'
+//   div.style.marginTop = '-3px'
+//   div.style.borderRadius = '50%'
+//   div.style.backgroundColor = 'blue'
+//   canvas.appendChild(div)
+// }
 
-canvas.onclick = (e) => {
-  console.log(e)
-  console.log(e.clientX)
-  console.log(e.clientY)
-  let div = document.createElement('div')
-  div.style.position = 'absolute'
-  div.style.left = e.clientX + 'px'
-  div.style.top = e.clientY + 'px'
-  div.style.width = '5px'
-  div.style.height = '5px'
-  div.style.marginLeft = '-3px'
-  div.style.marginTop = '-3px'
-  div.style.borderRadius = '50%'
-  div.style.backgroundColor = 'blue'
-  canvas.appendChild(div)
+let div = document.createElement('div')
+canvas.width = document.documentElement.clientWidth
+canvas.height = document.documentElement.clientHeight
+var ctx = canvas.getContext("2d")
+ctx.fillStyle = "blue"
+let painting = false
+
+canvas.onmousedown = () => {
+  painting = true
 }
+canvas.onmousemove = (e) => {
+  if (painting === true) {
+    ctx.beginPath();
+    ctx.arc(e.clientX - 6, e.clientY - 8, 10, 0, 2 * Math.PI)
+    ctx.fill()
+    // ctx.stroke()
+
+  } else {
+    console.log('Do noting')
+  }
+}
+
+canvas.onmouseup = () => {
+  painting = false
+}
+
+
+
+
