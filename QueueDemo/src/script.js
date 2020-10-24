32f6299a60b98633ffdd65a47893cb8604b94182
GraceYu <librayuyueran@gmail.com>
2020-10-23 23:48:38 -0500

    queue

---
+++ QueueDemo/src/script.js
@@ -0,0 +1,8 @@
+const divScreen = document.querySelector('#screen')
+const btnCreateNumber = document.querySelector('.createNumber')
+const btnCallNumber = document.querySelector('.callNumber')
+const showNewNumber = document.querySelector('#newNumber')
+const queue = document.querySelector('#queue')
+
+let n = 0;
+btnCreateNumber.onclick = () =>{}
