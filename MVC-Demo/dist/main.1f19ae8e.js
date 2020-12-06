f52ef61e8db29cd0016d6efc37af62e03e13fcf6
GraceYu <librayuyueran@gmail.com>
2020-12-06 12:49:11 -0600

    parcel again

--- MVC-Demo/dist/main.1f19ae8e.js
+++ MVC-Demo/dist/main.1f19ae8e.js
@@ -11413,7 +11413,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57716" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65002" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
