beddb9aeef0bdc0266e74694bf218662f9e92b43
GraceYu <librayuyueran@gmail.com>
2021-01-06 00:54:48 -0600

    webpack dev server

--- webpack_demo_1/webpack.config.js
+++ webpack_demo_1/webpack.config.js
@@ -8,8 +8,20 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.[contenthash].js'
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
   plugins: [new HtmlWebpackPlugin({
     title: 'Grace Yu Here is the Template!!!',
     template: 'src/assets/index.html'
-  })]
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
