0486f98510fc35e5e40f7a9e6210f08a465c4e32
GraceYu <librayuyueran@gmail.com>
2021-01-09 23:26:23 -0600

    mini-css-extract-plugi

--- webpack_demo_1/webpack.config.js
+++ webpack_demo_1/webpack.config.js
@@ -1,27 +1,44 @@
 const path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.[contenthash].js'
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
-  },
-  plugins: [new HtmlWebpackPlugin({
-    title: 'Grace Yu Here is the Template!!!',
-    template: 'src/assets/index.html'
-  })],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.[contenthash].js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Grace Yu Here is the Template!!!',
+            template: 'src/assets/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            // both options are optional
+            filename: '[name].[contenthash].css',
+            chunkFilename: '[id].[contenthash].css',
+        }),
     ],
-  },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: '/public/path/to/',
+                        },
+                    },
+                    'css-loader',
+                ],
+                // use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
 };
