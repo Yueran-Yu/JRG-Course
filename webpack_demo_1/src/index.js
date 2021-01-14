f368986883318dd358c5f967e9e8fd2de3e57710
GraceYu <librayuyueran@gmail.com>
2021-01-14 02:11:05 -0600

    lazy loading and import() function

--- webpack_demo_1/src/index.js
+++ webpack_demo_1/src/index.js
@@ -4,4 +4,18 @@ import png from './assets/01.png'
 console.log(png)
 console.log('hello here it is')
 const div = document.getElementById('app')
-div.innerHTML = `<div><img src="${png}"></div> `
+div.innerHTML = `<div><img src="${png}" width="300"></div> `
+
+const btn = document.createElement('button')
+btn.innerText = "lazy loading"
+btn.onclick = ()=>{
+    const promise = import('./0azy')
+    promise.then((fn)=>{
+        console.log('loaded successfully.')
+       const f =  fn.default
+        console.log(f)
+    }, ()=>{
+        console.log('An error occur when loading.')
+    })
+}
+div.appendChild(btn)
