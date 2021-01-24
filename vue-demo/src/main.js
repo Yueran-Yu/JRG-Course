e922252852bf24c553810f25bc27c973242178d9
GraceYu <librayuyueran@gmail.com>
2021-01-23 23:50:20 -0600

    vue-loader usage

--- vue-demo/src/main.js
+++ vue-demo/src/main.js
@@ -1,21 +1,19 @@
-console.log(window.Vue)
-console.dir(window.Vue)
+// console.log(window.Vue)
+// console.dir(window.Vue)
 
 // import Vue from 'vue'
 // import App from './App.vue'
 //
 // Vue.config.productionTip = false
-//
+
+import Demo from './Demo.vue'
+
+console.log("Demo===")
+console.log(Demo)
+
 new Vue({
-  el:"#app",
-  // template:`<div>{{n}}</div>`,
-  data:{
-    n:0
-  },
-  render(CreateElement){
-   return  CreateElement('div',[this.n, CreateElement('button',{on:{click:this.add}},'+1')])
-  },
-  methods:{
-    add(){ this.n += 1}
-  }
+    el:"#app",
+    render(createElement) {
+        return createElement(Demo)
+    }
 })
