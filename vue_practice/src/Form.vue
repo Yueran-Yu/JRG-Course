821874751789d288e63886db310badcf1d30c32d
GraceYu <librayuyueran@gmail.com>
2021-02-04 23:58:58 -0600

    input form

---
+++ vue_practice/src/Form.vue
@@ -0,0 +1,23 @@
+<template>
+<div id="app">
+  <input v-model="message" placeholder="edit me">
+  <p>Message is: {{ message }}</p>
+  <p>
+    <button @click="message =`frank`">Set Message To 2021</button>
+  </p>
+</div>
+</template>
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      message: 'hi'
+    }
+  },
+  components: {}
+}
+</script>
+
+
+
--- vue_practice/src/main.js
+++ vue_practice/src/main.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
-import App from './App.vue'
-import MyVue from "./MyVue.js"
+// import App from './App.vue'
+import Form from './Form.vue'
+// import MyVue from "./MyVue.js"
 
 Vue.config.productionTip = false
 
-new MyVue({
-  data(){
-    return {name:'Main'}
-  },
-  render: h => h(App),
+// new MyVue({
+//   data(){
+//     return {name:'Main'}
+//   },
+//   render: h => h(App),
+// }).$mount('#app')
+
+
+new Vue({
+  render: h => h(Form),
 }).$mount('#app')
