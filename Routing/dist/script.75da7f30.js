91a6587cfb9348f59751cad6973bc4ca8ab59000
GraceYu <librayuyueran@gmail.com>
2021-02-06 23:16:45 -0600

    sub routing

--- Routing/dist/script.75da7f30.js
+++ Routing/dist/script.75da7f30.js
@@ -118,36 +118,59 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"script.js":[function(require,module,exports) {
-var app = document.querySelector('#app'); //获取用户想去哪里
+var app = document.querySelector('#app');
+var div1 = document.createElement('div');
+div1.innerHTML = '1'; //嵌套路由
 
-var number = window.location.hash.substr(1) || 1; //获取界面
+var view1 = document.createElement('div');
+div1.appendChild(view1);
+view1.style.height = '50px';
+view1.style.background = 'pink';
+var div2 = document.createElement('div');
+div2.innerHTML = '2';
+var div3 = document.createElement('div');
+div3.innerHTML = '3';
+var div4 = document.createElement('div');
+div4.innerHTML = '4';
+var routeTable = {
+  "1": div1,
+  "2": div2,
+  "3": div3,
+  "4": div4
+};
+var div11 = document.createElement('div');
+div11.innerHTML = '1.1';
+var div12 = document.createElement('div');
+div12.innerHTML = '1.2';
+var div13 = document.createElement('div');
+div13.innerHTML = '1.3';
+var subRouteTable = {
+  "1/1": div11,
+  "1/2": div12,
+  "1/3": div13
+};
 
-var div = document.querySelector("#div".concat(number.toString())); //渲染界面
+function routing(container) {
+  var app = document.querySelector('#app'); //获取用户想去哪里
 
-if (div) {
-  div.style.display = "block";
-} else {
-  div = document.querySelector('#div404');
-  div.style.display = "block";
-} //展示界面，放到合适的位置
+  var number = window.location.hash.substr(1) || 1; //获取界面
 
+  var div = routeTable[number.toString()]; //渲染界面
 
-if (div) app.appendChild(div);
-window.addEventListener('hashchange', function () {
-  var number = window.location.hash.substr(1);
-  var div = document.querySelector("#div".concat(number.toString()));
-
-  if (div) {
-    div.style.display = "block";
-  } else {
+  if (!div) {
     div = document.querySelector('#div404');
-    div.style.display = "block";
   }
 
-  app.children[0].style.display = 'none';
-  document.body.appendChild(app.children[0]);
-  app.appendChild(div);
+  div.style.display = "block"; //展示界面，放到合适的位置
+
+  container.innerHTML = '';
+  if (container) app.appendChild(div);
+}
+
+window.addEventListener('hashchange', function () {
+  routing(app);
 });
+routing(app);
 },{}],"../../../../../.config/yarn/global/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -176,7 +199,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61007" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57372" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- Routing/src/script.js
+++ Routing/src/script.js
@@ -1,30 +1,56 @@
 const app = document.querySelector('#app')
-//获取用户想去哪里
-let number = window.location.hash.substr(1) || 1
-//获取界面
-let div = document.querySelector(`#div${number.toString()}`)
-//渲染界面
-if (div) {
-    div.style.display = "block"
-} else {
-    div = document.querySelector('#div404')
-    div.style.display = "block"
-}
-//展示界面，放到合适的位置
-if (div) app.appendChild(div)
+const div1 = document.createElement('div')
+div1.innerHTML = '1'
+//嵌套路由
+const view1 = document.createElement('div')
+div1.appendChild(view1)
+view1.style.height = '50px'
+view1.style.background = 'pink'
 
+const div2 = document.createElement('div')
+div2.innerHTML = '2'
+const div3 = document.createElement('div')
+div3.innerHTML = '3'
+const div4 = document.createElement('div')
+div4.innerHTML = '4'
+
+const routeTable = {
+    "1":div1,
+    "2":div2,
+    "3":div3,
+    "4":div4
+}
+const div11 = document.createElement('div')
+div11.innerHTML = '1.1'
+const div12 = document.createElement('div')
+div12.innerHTML = '1.2'
+const div13 = document.createElement('div')
+div13.innerHTML = '1.3'
+
+const subRouteTable={
+    "1/1":div11,
+    "1/2":div12,
+    "1/3":div13
+}
+
+function routing(container) {
+    const app = document.querySelector('#app')
+    //获取用户想去哪里
+    let number = window.location.hash.substr(1) || 1
+
+    //获取界面
+    let div = routeTable[number.toString()]
+    //渲染界面
+    if (!div) {
+        div = document.querySelector('#div404')
+    }
+    div.style.display = "block"
+    //展示界面，放到合适的位置
+    container.innerHTML = ''
+    if (container) app.appendChild(div)
+}
 
 window.addEventListener('hashchange', () => {
-    const number = window.location.hash.substr(1)
-    let div = document.querySelector(`#div${number.toString()}`)
-    if (div) {
-        div.style.display = "block"
-    } else {
-        div = document.querySelector('#div404')
-        div.style.display = "block"
-    }
-
-    app.children[0].style.display = 'none'
-    document.body.appendChild(app.children[0])
-    app.appendChild(div)
+    routing(app)
 })
+routing(app)
