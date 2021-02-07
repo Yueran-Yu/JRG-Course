44eb44a1e160a7ff917713e14637249ed80afe1b
GraceYu <librayuyueran@gmail.com>
2021-02-06 22:03:27 -0600

    default routing and Not foUND

--- Routing/dist/script.75da7f30.js
+++ Routing/dist/script.75da7f30.js
@@ -120,17 +120,30 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 })({"script.js":[function(require,module,exports) {
 var app = document.querySelector('#app'); //获取用户想去哪里
 
-var number = window.location.hash.substr(1); //获取界面
+var number = window.location.hash.substr(1) || 1; //获取界面
 
 var div = document.querySelector("#div".concat(number.toString())); //渲染界面
 
-if (div) div.style.display = "block"; //展示界面，放到合适的位置
+if (div) {
+  div.style.display = "block";
+} else {
+  div = document.querySelector('#div404');
+  div.style.display = "block";
+} //展示界面，放到合适的位置
+
 
 if (div) app.appendChild(div);
 window.addEventListener('hashchange', function () {
   var number = window.location.hash.substr(1);
   var div = document.querySelector("#div".concat(number.toString()));
-  div.style.display = "block";
+
+  if (div) {
+    div.style.display = "block";
+  } else {
+    div = document.querySelector('#div404');
+    div.style.display = "block";
+  }
+
   app.children[0].style.display = 'none';
   document.body.appendChild(app.children[0]);
   app.appendChild(div);
--- Routing/src/script.js
+++ Routing/src/script.js
@@ -1,18 +1,28 @@
 const app = document.querySelector('#app')
 //获取用户想去哪里
-const number = window.location.hash.substr(1)
+let number = window.location.hash.substr(1) || 1
 //获取界面
-const div = document.querySelector(`#div${number.toString()}`)
+let div = document.querySelector(`#div${number.toString()}`)
 //渲染界面
-if (div) div.style.display = "block"
+if (div) {
+    div.style.display = "block"
+} else {
+    div = document.querySelector('#div404')
+    div.style.display = "block"
+}
 //展示界面，放到合适的位置
 if (div) app.appendChild(div)
 
 
 window.addEventListener('hashchange', () => {
     const number = window.location.hash.substr(1)
-    const div = document.querySelector(`#div${number.toString()}`)
-    div.style.display = "block"
+    let div = document.querySelector(`#div${number.toString()}`)
+    if (div) {
+        div.style.display = "block"
+    } else {
+        div = document.querySelector('#div404')
+        div.style.display = "block"
+    }
 
     app.children[0].style.display = 'none'
     document.body.appendChild(app.children[0])
