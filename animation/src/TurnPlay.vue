fab95c62e498ea37188394f0257fea37eff67cd5
GraceYu <librayuyueran@gmail.com>
2021-02-07 23:59:16 -0600

    transition

---
+++ animation/src/TurnPlay.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <transition name="fade">
+      <button v-if="visible">Button</button>
+
+    </transition>
+    <hr>
+    <button @click="visible = !visible">Click</button>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      visible: false
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  .fade-enter-active, .fade-leave-active {
+    transition: all 2s;
+  }
+
+  .fade-enter,  .fade-leave-to  {
+    opacity: 0;
+  }
+}
+</style>
+
--- animation/src/main.js
+++ animation/src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import TurnPlay from './TurnPlay.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(TurnPlay),
 }).$mount('#app')
