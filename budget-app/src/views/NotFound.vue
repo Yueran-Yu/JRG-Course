bcbe952182811e10a886dec5609d7fe2aee6728d
GraceYu <librayuyueran@gmail.com>
2021-02-13 00:28:31 -0600

    page not found

---
+++ budget-app/src/views/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>404</h1>
+    <div>Page not found</div>
+    <router-link to="/">Back to Home Page</router-link>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "NotFound"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
