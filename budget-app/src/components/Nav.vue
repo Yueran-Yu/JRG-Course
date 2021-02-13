a3e7837bee994fdaaa59f7367dc60e94bd868b29
GraceYu <librayuyueran@gmail.com>
2021-02-13 01:50:45 -0600

    adjust footer

--- budget-app/src/components/Nav.vue
+++ budget-app/src/components/Nav.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="nav">
     <router-link to="/money">New</router-link>
     |
     <router-link to="/labels">Tags</router-link>
@@ -15,5 +15,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.nav{
+  border:1px solid pink;
+}
 </style>
--- budget-app/src/views/Money.vue
+++ budget-app/src/views/Money.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>Here is the Money Page
+  <div class="nav-wrapper">
+    <div class="content">
+      Here is the Money Page
+    </div>
     <Nav/>
   </div>
 </template>
@@ -12,4 +15,16 @@ export default {
 
 <style lang="scss" scoped>
 
+
+.nav-wrapper {
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.content {
+  background-color: #9c9cdb;
+  flex-grow: 1;
+}
 </style>
