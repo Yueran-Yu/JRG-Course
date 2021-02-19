17417929cae423f6145e05a3620e44dcbf270c26
GraceYu <librayuyueran@gmail.com>
2021-02-18 20:19:21 -0600

    layout of keyboard

--- budget-app/src/views/Money.vue
+++ budget-app/src/views/Money.vue
@@ -44,7 +44,6 @@
           <button class="ok">OK</button>
           <button class="zero">0</button>
           <button>.</button>
-          <button>%</button>
           <button>÷</button>
         </div>
       </div>
@@ -63,22 +62,70 @@ export default {
 
 .numberPad {
   .output {
+    @extend %clearFix;
+    @extend %innerShadow;
     font-size: 30px;
     padding: 9px 16px;
     font-family: Consolas, monospace;
     text-align: right;
+
   }
 
   .buttons {
+    @extend %clearFix;
+
     button {
-      width:20%;
+      width: 20%;
       outline: none;
-      border:none;
-      height:64px;
+      border: none;
+      background: transparent;
+      height: 64px;
       float: left;
-      &.ok{
-        height:64*2px;
+
+      &.ok {
+        height: 64*2px;
         float: right;
+      }
+
+      &.zero {
+        width: 20*2%;
+      }
+
+      $bg: #fffcfd;
+
+      &:nth-child(1) {
+        background: $bg;
+      }
+
+      &:nth-child(2), &:nth-child(6) {
+        background: darken($bg, 4%);
+      }
+
+      &:nth-child(3), &:nth-child(7), &:nth-child(11) {
+        background: darken($bg, 8%);
+      }
+
+      &:nth-child(4), &:nth-child(8), &:nth-child(12) {
+        background: darken($bg, 12%);
+      }
+      &:nth-child(16){
+        background: darken($bg,14%);
+      }
+
+      &:nth-child(5), &:nth-child(9), &:nth-child(13) {
+        background: darken($bg, 16%);
+      }
+
+      &:nth-child(10), &:nth-child(14), &:nth-child(17) {
+        background: darken($bg, 20%);
+      }
+
+      &:nth-child(18) {
+        background: darken($bg, 24%);
+      }
+
+      &:nth-child(15) {
+        background: darken($bg, 26%);
       }
     }
   }
