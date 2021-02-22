34e8f2964faac31581aaa84f452223aef17e11dd
GraceYu <librayuyueran@gmail.com>
2021-02-21 22:19:22 -0600

    version migration

--- budget-app/src/views/Money.vue
+++ budget-app/src/views/Money.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <Layout class-prefix="layout">
+      {{ recordList }}
       <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
       <Types :valueType.sync="record.type"/>
       <Notes @update:value="onUpdateNotes"/>
@@ -16,11 +17,25 @@ import Types from "@/components/Money/Types.vue";
 import Notes from "@/components/Money/Notes.vue";
 import Tags from "@/components/Money/Tags.vue";
 
+const version = window.localStorage.getItem('version') || '0';
+const recordList: Record[] = JSON.parse(window.localStorage.getItem('version')||'[]')
+if (version === '0.0.1') {
+  //数据库升级，数据迁移
+  recordList.forEach(record => {
+    record.createAt = new Date(2020, 0, 1)
+  })
+  // 保存数据
+  window.localStorage.setItem('recordList', JSON.stringify(recordList))
+}
+window.localStorage.setItem('version', '0.0.2')
+
 type Record = {
   tags: string[];
   notes: string;
   type: string;
-  amount: number;
+  amount: number; // data type => object / string
+  // createdAt?: Date; // class/constructor=> class is used to divide objects
+  // 问号的意思是 createAt 可以不存在
 }
 
 @Component({
@@ -29,7 +44,7 @@ type Record = {
 export default class Money extends Vue {
   tags = ['Residence', 'Food', 'Wear', 'Transportation', 'Utilities']
   record: Record = {tags: [], notes: '', type: '-', amount: 0};
-  recordList: Record[] = []
+  recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]')
 
   onUpdateTags(tags: string[]) {
     this.record.tags = tags
@@ -40,14 +55,15 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
-    const deepClone = JSON.parse(JSON.stringify(this.record))
-    this.recordList.push(deepClone)
-    console.log(this.recordList)
+    const record2: Record = JSON.parse(JSON.stringify(this.record))
+    // record2.createdAt = new Date()
+    this.recordList.push(record2)
+    // console.log(this.recordList)
   }
 
   @Watch('recordList')
   onRecordListChanged() {
-    window.localStorage .setItem('recordList', JSON.stringify(this.recordList))
+    window.localStorage.setItem('recordList', JSON.stringify(this.recordList))
   }
 }
 </script>
