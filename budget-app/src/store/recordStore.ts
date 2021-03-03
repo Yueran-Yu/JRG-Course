7659e60a8d5bc3a3a24baeea48df99f15aa8ad48
GraceYu <librayuyueran@gmail.com>
2021-03-02 23:37:00 -0600

    delete models folder

--- budget-app/src/store/recordStore.ts
+++ budget-app/src/store/recordStore.ts
@@ -14,9 +14,11 @@ const recordStore = {
         window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.recordList));
     },
     // create record
-    createRecord(record: RecordItem){
+    createRecord(record: RecordItem) {
         const record2: RecordItem = clone(record)
         record2.createdAt = new Date()
+        // this.recordList?.push(record2)
+        //可选连语法
         this.recordList && this.recordList.push(record2)
         recordStore.saveRecords();
     },
