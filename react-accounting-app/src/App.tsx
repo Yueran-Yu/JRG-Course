487eb3813a25549973594fe66af05896df9bb47a
GraceYu <librayuyueran@gmail.com>
2021-07-02 21:04:23 -0500

    remove no need files

--- react-accounting-app/src/App.tsx
+++ react-accounting-app/src/App.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
-import './App.css';
-import styled from 'styled';
-
-
-const Button = styled.button`
-color:grey`;
 
 
 function App() {
-  return (
-    <div className="App">
-
-
-    </div>
-  );
+    return (
+        <div className="App">
+        </div>
+    );
 }
 
 export default App;
