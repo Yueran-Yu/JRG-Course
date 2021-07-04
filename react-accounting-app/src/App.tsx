0d9ad901bdebb4af61f1cba270bed96897a5d96b
GraceYu <librayuyueran@gmail.com>
2021-07-03 23:33:55 -0500

    update

--- react-accounting-app/src/App.tsx
+++ react-accounting-app/src/App.tsx
@@ -1,11 +1,58 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect
+} from "react-router-dom";
 
 
 function App() {
     return (
-        <div className="App">
-        </div>
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/tags">Tags</Link>
+                        </li>
+                        <li>
+                            <Link to="/money">Money</Link>
+                        </li>
+                        <li>
+                            <Link to="/statistics">Statistics</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                <Switch>
+                    <Route path="/tags">
+                        <Tags/>
+                    </Route>
+                    <Route path="/money">
+                        <Money/>
+                    </Route>
+                    <Route path="/statistics">
+                        <Statistics/>
+                    </Route>
+                    <Redirect  from='/' to="/money"/>
+                </Switch>
+            </div>
+        </Router>
     );
+}
+
+function Tags() {
+    return <h2>Home</h2>;
+}
+
+function Money() {
+    return <h2>Money</h2>;
+}
+
+function Statistics() {
+    return <h2>Statistics</h2>;
 }
 
 export default App;
---
+++ react-accounting-app/src/a.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export const Home = () => <div>This is home component</div>
+export const Test1 = () => <div>This is Test1 component</div>
+export const Test2 = () => <div>This is Test2 component</div>
+export const Test3 = () => <div>This is Test3 component</div>;
