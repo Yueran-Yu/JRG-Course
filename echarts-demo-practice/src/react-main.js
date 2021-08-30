c58c6b027dd8c58ed67dfdd95a8ebb6dff9e45bf
GraceYu <librayuyueran@gmail.com>
2021-08-30 12:58:05 -0500

    update

--- echarts-demo-practice/src/react-main.js
+++ echarts-demo-practice/src/react-main.js
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {ReactApp} from "./react-app";
 
+
+// I used git push --force
 ReactDOM.render(<ReactApp/>, document.querySelector('#main'))
